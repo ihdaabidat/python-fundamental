2f0dfc01b484c5c1a730975887d59ec843415307
.git <ihdaabidat19@gmail.com>
2024-06-20 21:55:04 +0700

    Initial commit

--- main.py
+++ main.py
@@ -13,10 +13,23 @@ print("Maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
 
 # Percabangan
-jumlah_botol_susu = 0
+jumlah_botol_susu = 173
+jumlah_telur = 1587
+print(f"Jumlah botol susu {jumlah_botol_susu} botol")
+print(f"Jumlah telur {jumlah_telur} butir")
 
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya, dan ternyata uangnya cukup")
     print("Budi membeli 1 botol susu")
 else:
     print("Budi tidak jadi membeli 1 botol susu")
+
+if jumlah_telur > 0:
+    print("Budi mengecek apakah ada telur, dan ternyata telurnya ada")
+    print("Budi membeli 6 butir telur")
+else:
+    print("Budi tidak jadi membeli 6 butir telur")
+
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada ibu")
+
