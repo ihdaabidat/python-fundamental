294c8bd8131e5dc3438718dda3995a5b0ad25dbd
.git <ihdaabidat19@gmail.com>
2024-06-21 14:51:27 +0700

    singkatanvariabel

--- dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
@@ -1,18 +1,26 @@
 """
 Program perulangan membaca buku dengan while sampai paham
+Perulangan dengan while dengan jumlah tak terbatas dan ada variabel pemberhenti
 """
 jumlah_buku = 10
 print('Ibu berkata, "Baca semua bukumu"')
+jumlah_baca = 0
 
-jumlah_buku_sudah_dibaca_dan_dipahami = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
+jumlah_paham = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}')
 
 print("Dengan while")
-while jumlah_buku_sudah_dibaca_dan_dipahami < jumlah_buku:
-    if jumlah_buku_sudah_dibaca_dan_dipahami == 10:
-        print(f"Buku ke {jumlah_buku_sudah_dibaca_dan_dipahami} belum paham")
+while jumlah_baca < jumlah_buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 9:
+        print(f"Buku ke {jumlah_paham + 1} belum paham")
     else:
-        jumlah_buku_sudah_dibaca_dan_dipahami = jumlah_buku_sudah_dibaca_dan_dipahami + 1
-        print(f"Buku ke {jumlah_buku_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+        jumlah_paham = jumlah_paham + 1
+        print(f"Buku ke {jumlah_paham} sudah dibaca dan dipahami")
 
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}')
+if jumlah_paham == jumlah_buku:
+    print('"Bu semua buku sudah dibaca dan dipahami"')
+else:
+    print(f'"Bu tidak semua buku bisa dipahami. Budi hanya bisa memahami {jumlah_paham} buku"')
+
