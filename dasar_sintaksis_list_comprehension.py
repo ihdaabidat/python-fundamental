1c8e653acb5c3a5001059c88e2255fc6c2792784
.git <ihdaabidat19@gmail.com>
2024-07-07 15:29:50 +0700

    Updated

--- dasar_sintaksis_list_comprehension.py
+++ dasar_sintaksis_list_comprehension.py
@@ -29,3 +29,14 @@ del daftar_buku[0::2] #START:END:STEP
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
+print('\nMembuat list baru')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First']
+daftar_buku_baru = daftar_buku[:]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+# meskipun daftar_buku dihapus tapi daftar_buku_baru itu sudah menjadi list yang terpisah sehingga dia ga akan terpengaruh (tempatnya udah berbeda)
+print('\nMembuat list baru')
+del daftar_buku[:]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
