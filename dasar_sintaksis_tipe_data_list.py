a3dece8019d749a5c3b21fceb1d23443978423dd
.git <ihdaabidat19@gmail.com>
2024-07-07 15:04:45 +0700

    Updated

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -61,34 +61,3 @@ for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\nVariabel yang sering di pake i, j, k')
-
-print('\nPerintah del')
-daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
-del daftar_buku[0]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-### Menggunakan List Comprehension ###
-print('\nPerintah del dengan List comprehension')
-daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
-del daftar_buku[:]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nPerintah del dengan List comprehension start')
-daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
-del daftar_buku[0:3] #START:END
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nPerintah del dengan List comprehension start -1')
-daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
-del daftar_buku[0:-2] #START:END
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nPerintah del dengan List comprehension start:end:step')
-daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First']
-del daftar_buku[0::2] #START:END:STEP
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
