f1d2eeebf21bec119b4607df9793161dc1a0b2a2
.git <ihdaabidat19@gmail.com>
2024-07-13 21:31:59 +0700

    Updated

--- dasar_sintaksis_list_comprehension.py
+++ dasar_sintaksis_list_comprehension.py
@@ -3,15 +3,22 @@ daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', 
 del daftar_buku[0]
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
-
+# index dimulai dari 0
+# jumlah dimulai dari 1
 ### Menggunakan List Comprehension ###
-print('\nPerintah del dengan List comprehension')
+print('\nPerintah del dengan list comprehension')
 daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
-del daftar_buku[:]
+del daftar_buku[:] # [:] list comprehension
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-print('\nPerintah del dengan List comprehension start')
+print('\nPerintah del dengan list comprehension')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku[0:0] # [:] list comprehension
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah del dengan list comprehension start')
 daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
 del daftar_buku[0:3] #START:END
 for i in range(0, len(daftar_buku)):
@@ -19,18 +26,18 @@ for i in range(0, len(daftar_buku)):
 
 print('\nPerintah del dengan List comprehension start -1')
 daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
-del daftar_buku[0:-2] #START:END
+del daftar_buku[0:-2] #START:END #-2 artinya kamu akan menghapus dari semau dan menyisakan 2 dari urutan terakhir
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\nPerintah del dengan List comprehension start:end:step')
-daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First']
-del daftar_buku[0::2] #START:END:STEP
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku[0::2] #START:END:STEP  #comprehension [::] kelipatan kedua
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\nMembuat list baru')
-daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First']
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
 daftar_buku_baru = daftar_buku[:]
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
@@ -40,3 +47,25 @@ print('\nMembuat list baru')
 del daftar_buku[:]
 for i in range(0, len(daftar_buku_baru)):
     print(daftar_buku_baru[i])
+
+print('\nMembuat list baru dengan comprehension: ganjil')
+daftar_buku = ['1 Seven Habits', '2 How to Influence People', '3 First Things First', '4 4DX']
+daftar_buku_baru = daftar_buku[0::2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat list baru dengan comprehension: genap')
+daftar_buku = ['1 Seven Habits', '2 How to Influence People', '3 First Things First', '4 4DX']
+daftar_buku_baru = daftar_buku[1::2] #start stop end
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat list baru dengan comprehension: buang di ujung')
+daftar_buku = ['1 Seven Habits', '2 How to Influence People', '3 First Things First', '4 4DX']
+daftar_buku_baru = daftar_buku[0:-1:2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat list baru dengan comprehension: ganjil')
+daftar_buku = ['1 Seven Habits', '2 How to Influence People', '3 First Things First', '4 4DX']
+print(daftar_buku[0::2])
